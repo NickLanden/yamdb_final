5b9a01ec61f8219ffbf395166862f150991c05b7
n.lopatkin <n1cklanden@yandex.ru>
2022-07-11 14:10:38 +0300

    add random secret key in settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,6 @@
 import os
 from datetime import timedelta
+from django.core.management.utils import get_random_secret_key
 
 from dotenv import load_dotenv
 
@@ -7,7 +8,7 @@ load_dotenv(dotenv_path='../infra/.env')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
 DEBUG = False
 
